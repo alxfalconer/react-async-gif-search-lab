147dc7d0a4f8d7061a9cffa52c3868b4fc345a4c
Alexander <al.x.falconer@gmail.com>
2022-03-25 14:00:00 +0100

    done

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -3,6 +3,14 @@ import React from "react";
 export default class GifSearch extends React.Component {
   render() {
     return (
+        <div className="form-row align-items-center">
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="Search GIFs"
+          value={this.props.formData.search}
+          onChange={this.props.formUpdate}
+        />
         <button
           type="button"
           className="btn btn-primary btn-block"
@@ -10,6 +18,7 @@ export default class GifSearch extends React.Component {
         >
           Search
         </button>
+        </div>
     
     );
   }
